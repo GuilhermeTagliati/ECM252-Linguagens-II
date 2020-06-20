70a22e96895a3928059082d219b8b6b9610a4bab
Guilherme Tagliati <guilhermetagliati@outlook.com>
2020-06-20 15:38:07 -0300

    First Time Angular V0.1.1

--- project-maua/src/app/app.module.ts
+++ project-maua/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent,
+    FooterComponentComponent,
   ],
   imports: [
     BrowserModule
